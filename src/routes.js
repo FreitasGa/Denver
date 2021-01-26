d7d48332294c628d587fd3d93c57f54cd731f25e
mariliaeuridesbr <67061731+mariliaeuridesbr@users.noreply.github.com>
2021-01-25 23:28:22 -0300

    Made some changes to routes

--- src/routes.js
+++ src/routes.js
@@ -29,8 +29,7 @@ function Routes() {
         <Route path="/login" component={Login} />
         <PrivateRoute path="/perfil" component={Perfil} />
         <PrivateRoute path="/aulas" component={Aulas} />
-        <PrivateRoute path="/lessons/1" component={Video} />
-        <PrivateRoute path="/lessons/2" component={Video} />
+        <PrivateRoute path="/lessons" component={Video} />
         <Route path="/landing" component={LandingPage} />
         <PrivateRoute path="/questionario" component={Questionario} />
       </Switch>
